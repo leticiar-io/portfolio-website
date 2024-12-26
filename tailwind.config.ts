01b98fafb7f49d5dc5fed51ca696251388a19fdf
leticiar-io <leticia.rodrigues20019@gmail.com>
2024-12-25 23:52:34 -0300

    feat: Configured Tailwind CSS settings in tailwind.config.ts and global styles in globals.css

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,9 +8,34 @@ export default {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        anton: ['Anton', 'sans-serif'],
+        playball: ['Playball', 'sans-serif'],
+        poppins: ['Poppins', 'sans-serif'],
+      },
+      borderWidth: {
+        '3': '2.2px',
+      },
+      borderRadius: {
+        'sm': '0.3125rem',
+      },
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: 'var(--background)',
+        gray: {
+          100: 'var(--gray-100)',
+          200: 'var(--gray-200)',
+          300: 'var(--gray-300)',
+        },
+        black: {
+          100: 'var(--black-100)',
+          200: 'var(--black-200)',
+          300: 'var(--black-300)',
+          400: 'var(--black-400)',
+          600: 'var(--black-600)',
+          700: 'var(--black-700)',
+          800: 'var(--black-800)',
+        },
+        yellow: 'var(--yellow)',
       },
     },
   },
